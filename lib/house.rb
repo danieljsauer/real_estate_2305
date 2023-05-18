3e254f8a79089749a48523ee874c154cbbc6446f
Daniel Sauer <danieljsauer92@gmail.com>
2023-05-18 10:20:20 -0600

    clarify tests and create house object to complete iteration 2

---
+++ lib/house.rb
@@ -0,0 +1,16 @@
+class House
+  
+  attr_reader :price, :address
+  attr_accessor :rooms 
+
+  def initialize(price, address)
+    @price = price.delete("$").to_i 
+    @address = address 
+    @rooms = [] 
+
+  end
+  
+  def add_room(room)
+    rooms << room 
+  end 
+end
--- spec/house_spec.rb
+++ spec/house_spec.rb
@@ -22,21 +22,23 @@ RSpec.describe House do
       house = House.new("$400000", "123 sugar lane")
       expect(house.rooms).to eq([])
     end 
+
     it "can add rooms into the house" do 
       house = House.new("$400000", "123 sugar lane")
       room_1 = Room.new(:bedroom, 10, '13')
-      expect(house.room_1).to be_a Room 
-      expect(house.room_1.category).to eq(:bedroom)
-      expect(rooms.count).to eq(1)
+      house.add_room(room_1)
+      expect(house.rooms[0]).to be_a Room 
+      expect(house.rooms.count).to eq(1)
     end 
+
     it "can add multiple rooms" do 
       house = House.new("$400000", "123 sugar lane")
       room_1 = Room.new(:bedroom, 10, '13')
       room_2 = Room.new(:bedroom, 11, '15')   
-      expect(rooms.count).to eq(2)
+      house.add_room(room_1)
+      house.add_room(room_2)
+      expect(house.rooms.count).to eq(2)
     end 
   end 
-
-
 
 end
