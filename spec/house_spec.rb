89ad86dcd47bdb716bbf729b6b2dac44d2096ad5
Daniel Sauer <danieljsauer92@gmail.com>
2023-05-18 10:02:56 -0600

    create house object tests for iteration 2

--- spec/house_spec.rb
+++ spec/house_spec.rb
@@ -8,6 +8,35 @@ RSpec.describe House do
       expect(house).to be_a House
     end 
 
+    it "has a price" do 
+      house = House.new("$400000", "123 sugar lane")
+      expect(house.price).to eq(400000)
+    end 
+
+    it "had an address" do 
+      house = House.new("$400000", "123 sugar lane")
+      expect(house.address).to eq("123 sugar lane")
+    end
+
+    it "rooms default value is an empty array" do 
+      house = House.new("$400000", "123 sugar lane")
+      expect(house.rooms).to eq([])
+    end 
+    it "can add rooms into the house" do 
+      house = House.new("$400000", "123 sugar lane")
+      room_1 = Room.new(:bedroom, 10, '13')
+      expect(house.room_1).to be_a Room 
+      expect(house.room_1.category).to eq(:bedroom)
+      expect(rooms.count).to eq(1)
+    end 
+    it "can add multiple rooms" do 
+      house = House.new("$400000", "123 sugar lane")
+      room_1 = Room.new(:bedroom, 10, '13')
+      room_2 = Room.new(:bedroom, 11, '15')   
+      expect(rooms.count).to eq(2)
+    end 
   end 
+
+
 
 end
