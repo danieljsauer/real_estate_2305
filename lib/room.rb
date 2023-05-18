bd1d14c7e466b84eb534ef3bed530d4daa7b22ac
Daniel Sauer <danieljsauer92@gmail.com>
2023-05-18 09:42:18 -0600

    create tests and room object for iteration 1

--- lib/room.rb
+++ lib/room.rb
@@ -1,6 +1,26 @@
 class Room
 
-  def initialize
+  attr_reader :category, :length, :width, :painted 
 
+  def initialize(category, length, width)
+    @category = category.to_sym
+    @length = length
+    @width = width 
+    @painted = false 
   end
+
+  def area
+    width_int = width.to_i
+    area = length * width_int  
+    area 
+  end 
+
+  def is_painted?
+    @painted
+  end 
+
+  def paint 
+    @painted = true 
+  end 
+
 end
